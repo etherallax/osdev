7dca99e3ed690918f242ccb197bbfa1ef7dced8d
etherallax <etherallax@gmail.com>
2026-02-27 09:19:58 -0500

    Update another_example.asm
    Removed useless disk reading routines.

--- another_example.asm
+++ another_example.asm
@@ -9,25 +9,7 @@ msg db "Welcome to this OS", 0ah, 0dh, 0h
 boot:
   cli       ; clear interrupts (allows system to respond to i/o)
   cld       ; all we need to init
-  
-  mov ax, 0x50
-
-  ; set the buffer
-  mov es, ax 
-  xor bx, bx
-
-  mov al, 2     ; read 2 sector
-  mov ch, 0     ; track 0
-  mov cl, 2     ; sector to read
-  mov dh, 0     ; head number
-  mov dl, 0     ; drive number
-
-  mov ah, 0x02  ; read sectors from disk
-  
-  int 0x13      ; call BIOS routine
-  jmp 0x50:0x0  ; jump and execute the sector
-
-  hlt           ; halt the system
+  hlt          ; halt the system
 
 ; We have to be 512 bytes. Clear the rest of the bytes with 0.
 times 510 - ($-$$) db 0
